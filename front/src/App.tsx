9a14eb254c147c4697f5d85d07178307b460349c
malak <benaounmalak98@gmail.com>
2022-07-22 17:34:29 +0100

    feat: add `routes` in `app` component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,13 +4,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Comments } from "./components/Comments";
 import { Main } from "./components/Main";
+import { ToastContainer } from "react-toastify";
+import { Register } from "./components/Register";
+import { Login } from "./components/Login";
+import { ListComments } from "./components/ListComments";
 
 export function App() {
   return (
     <BrowserRouter>
+      <ToastContainer />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Main />} />
         <Route path="/comments" element={<Comments />} />
+        <Route path="/listComments" element={<ListComments />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
