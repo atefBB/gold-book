ba133436bdb320d1da9924d6ea44bf86454d82b5
malak <benaounmalak98@gmail.com>
2022-07-24 14:04:47 +0100

    feat: add `on navigate login` function and `disconnect` button in `header` component

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-
 import { useNavigate } from "react-router-dom";
 export function Header() {
   const navigate = useNavigate();
@@ -8,12 +7,21 @@ export function Header() {
   function onNavigateComments() {
     navigate("/comments");
   }
+  function onNavigateLogin() {
+    navigate("/");
+    localStorage.clear()
+  }
   return (
     <header className="header">
+      <img src={"../assets/react-logo.png"} className="header--image" alt="logo" />
+
       <h2 className="header--title">GOLD BOOK</h2>
       <Button className="form--button">HOME</Button>
       <Button className="form--button" onClick={onNavigateComments}>
         COMMENTS
+      </Button>
+      <Button className="form--button" onClick={onNavigateLogin}>
+        DISCONNECT
       </Button>
     </header>
   );
