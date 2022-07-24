21a92481a8459bace23e34e7f2ee6e232b2f101d
malak <benaounmalak98@gmail.com>
2022-07-24 14:13:32 +0100

    feat: add `user comments`, `register` and `login` routes in `commande router` file

--- server/src/router/CommandeRouter.ts
+++ server/src/router/CommandeRouter.ts
@@ -3,6 +3,9 @@ import { postComment } from "../controllers/postComment";
 import { getComments } from "../controllers/getComments";
 import { deleteComment } from "../controllers/deleteComment";
 import { updateComment } from "../controllers/updateComment";
+import { postUser } from "../controllers/postUser";
+import { login } from "../controllers/login";
+import { getUserComments } from "../controllers/getUserComment";
 
 const router = express.Router();
 
@@ -42,7 +45,42 @@ const router = express.Router();
  */
 router.route("/comments").get(getComments);
 
-
+/**
+ * @swagger
+ * /user_comments:
+ *   post:
+ *     summary: get user comments
+ *     tags: [User]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id_user:
+ *                type: string
+ *     responses:
+ *       200:
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       404:
+ *         description: not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       500:
+ *         description: error
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *
+ */
+router.route("/user_comments").post(getUserComments);
 
 /**
  * @swagger
@@ -95,6 +133,8 @@ router.route("/deleteComment").delete(deleteComment);
  *             type: object
  *             properties:
  *               comment:
+ *                 type: string
+ *               id_user:
  *                 type: string
  *     responses:
  *       200:
@@ -158,6 +198,86 @@ router.route("/add_comment").post(postComment);
 
 router.route("/update_comment").put(updateComment);
 
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: add user
+ *     tags: [User]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               role:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       404:
+ *         description: not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       500:
+ *         description: error
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *
+ */
+router.route("/register").post(postUser);
 
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: get user
+ *     tags: [User]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+  *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: ok
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       404:
+ *         description: not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *       500:
+ *         description: error
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *
+ */
+router.route("/login").post(login);
 
 export default router;
