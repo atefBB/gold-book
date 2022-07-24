3a4c404d4cf916107082a052e4f4aac89093ee24
malak <benaounmalak98@gmail.com>
2022-07-24 14:10:49 +0100

    fix: fix `post comment` function in `post comment` file

--- server/src/controllers/postComment.ts
+++ server/src/controllers/postComment.ts
@@ -5,10 +5,11 @@ import pool from "../database";
 export const postComment = async (request: Request, response: Response) => {
 
   try {
-    const { comment } = request.body;
-    let sql = `INSERT INTO public.comments (comment) VALUES ('${comment}') RETURNING * `
-    const newComment = await pool.query(sql
-    );
+    const { comment, id_user } = request.body;
+    let sql = `INSERT INTO public.comments (comment, id_user) VALUES ('${comment}', '${id_user}') RETURNING * `
+    const newComment = await pool.query(sql);
+    console.log(sql);
+
     response.json(newComment.rows);
   } catch (error) {
     console.log(error);
