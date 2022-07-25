ce7ce047329e802229c5f12c5d3d581fbac29078
malak <benaounmalak98@gmail.com>
2022-07-25 15:30:57 +0100

    chore: clean-up `comments` component

--- front/src/components/Comments.tsx
+++ front/src/components/Comments.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import { Header } from "./Header";
 
 export function Comments() {
   const [comments, setComments] = React.useState([]);
   const [comment, setComment] = React.useState("");
-
+  
   async function getComments() {
     await fetch(`${process.env.REACT_APP_API_URL}/user_comments`, {
       method: "POST",
@@ -25,18 +26,15 @@ export function Comments() {
   }
   const handleFormUpdateEvent = async (idc: any) => {
     try {
-      const Response = await fetch(
-        `${process.env.REACT_APP_API_URL}/update_comment`,
-        {
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            id: idc,
-            comment: comment,
-          }),
-        }
-      );
-      console.log(Response);
+      await fetch(`${process.env.REACT_APP_API_URL}/update_comment`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          id: idc,
+          comment: comment,
+        }),
+      });
+
       alert("Comment updated successfully");
     } catch (err) {
       console.error(err);
@@ -59,9 +57,10 @@ export function Comments() {
     getComments();
   });
 
-  console.log(comments);
+  
   return (
-    <>
+    <React.Fragment>
+      <Header/>
       <table className="table">
         <thead>
           <tr>
@@ -75,72 +74,67 @@ export function Comments() {
             <tr key={Element.id}>
               <td>{Element.comment}</td>
               <td>
-                <>
-                  <Button
-                    type="button"
-                    className="btn btn-warning"
-                    data-toggle="modal"
-                    data-target={`#id${Element.id}`}
-                    onClick={() => setComment(Element.comment)}
-                  >
-                    Edit
-                  </Button>
+                <Button
+                  type="button"
+                  className="btn btn-warning"
+                  data-toggle="modal"
+                  data-target={`#id${Element.id}`}
+                  onClick={() => setComment(Element.comment)}
+                >
+                  Edit
+                </Button>
 
-                  <div className="modal" id={`id${Element.id}`}>
-                    <div className="modal-dialog">
-                      <div className="modal-content">
-                        <div className="modal-header">
-                          <h4 className="modal-title">Edit Comment</h4>
-                          <Button
-                            type="button"
-                            className="close"
-                            data-dismiss="modal"
-                            onClick={() => setComment(Element.comment)}
-                          >
-                            &times;
-                          </Button>
-                        </div>
+                <div className="modal" id={`id${Element.id}`}>
+                  <div className="modal-dialog">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h4 className="modal-title">Edit Comment</h4>
+                        <Button
+                          type="button"
+                          className="close"
+                          data-dismiss="modal"
+                          onClick={() => setComment(Element.comment)}
+                        >
+                          &times;
+                        </Button>
+                      </div>
 
-                        <div className="modal-body">
-                          <input
-                            type="text"
-                            className="form-control"
-                            placeholder="edit comment"
-                            value={comment}
-                            onChange={(e: any) => setComment(e.target.value)}
-                          />
-                        </div>
+                      <div className="modal-body">
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="edit comment"
+                          value={comment}
+                          onChange={(e: any) => setComment(e.target.value)}
+                        />
+                      </div>
 
-                        <div className="modal-footer">
-                          <Button
-                            type="button"
-                            className="btn btn-warning"
-                            data-dismiss="modal"
-                            onClick={(): any =>
-                              handleFormUpdateEvent(Element.id)
-                            }
-                          >
-                            Edit
-                          </Button>
-                          <Button
-                            type="button"
-                            className="btn btn-danger"
-                            data-dismiss="modal"
-                          >
-                            Close
-                          </Button>
-                        </div>
+                      <div className="modal-footer">
+                        <Button
+                          type="button"
+                          className="btn btn-warning"
+                          data-dismiss="modal"
+                          onClick={(): any => handleFormUpdateEvent(Element.id)}
+                        >
+                          Edit
+                        </Button>
+                        <Button
+                          type="button"
+                          className="btn btn-danger"
+                          data-dismiss="modal"
+                        >
+                          Close
+                        </Button>
                       </div>
                     </div>
                   </div>
-                </>
+                </div>
               </td>
               <td>
                 <Button
                   className="btn btn-danger"
                   onClick={() => {
                     handleFormDeleteEvent(Element.id);
-                    console.log(Element.id);
                   }}
                 >
                   Delete
@@ -150,6 +144,6 @@ export function Comments() {
           ))}
         </tbody>
       </table>
-    </>
+    </React.Fragment>
   );
 }
